29fb6af969be86fae6b94052128c0767713bc627
Francine Cruz <francine.c.oliveiracruz@gmail.com>
2024-01-14 12:36:12 -0300

    Adicionando movimento e outras funcionalidades.

--- script.js
+++ script.js
@@ -1,15 +1,52 @@
-let runing = document.querySelector('#runing');
+const runing = document.querySelector('#runing');
+const stone = document.querySelector('#stone');
+const gameOver = document.querySelector('#game-over');
+const stop = document.querySelector('#stop');
+const time = document.querySelector('#time');
+const music = document.getElementById('music');
+
+music.play();
+
+let timeElapsed = 0;
 
 let jumping = () => {
     runing.classList.add('jumping');
+    
+    runing.classList.remove('jumping');
 
-    setTimeout(() => {
-        runing.classList.remove('jumping');
-    }, 600);
+    void runing.offsetWidth;
+
+    runing.classList.add('jumping');
 }
 
-const loop = setInterval (() => {
-    const pipeLocal = pipe.offsetLeft;
-}, 12);
+let Collision = setInterval(() => {
+    const stoneLocal = stone.offsetLeft;
+    const runingLocal = parseInt(window.getComputedStyle(runing).bottom.replace('px', ''));
 
-document.addEventListener('keydown', jumping);
+    if (stoneLocal <= 120 &&  
+        runingLocal <= 74 && 
+        stoneLocal > 0
+    ) {
+        gameOver.style.display = 'block';
+
+        stone.style.animation = 'none';
+        stone.style.left = stoneLocal + 'px';
+        stone.style.animationPlayState = 'paused';
+        
+        runing.style.animation = 'none';
+        runing.style.bottom = runingLocal + 'px';
+        
+
+        runing.replaceWith(stop);
+        stop.style.display = 'block';
+
+        clearInterval (Collision);
+        music.pause();
+    }  
+
+    timeElapsed ++;
+    time.textContent = `Percorrido: ${timeElapsed}s`;
+
+}, 9);
+
+document.addEventListener ('keydown', jumping); 
